bb0f189253789461241848e01ba47b5a6f1ca0be
chen3082 <46243863+chen3082@users.noreply.github.com>
2019-10-21 19:30:52 -0400

    Update __init__.py

--- flappy/envs/fwmav/pydart2/gui/__init__.py
+++ flappy/envs/fwmav/pydart2/gui/__init__.py
@@ -4,9 +4,8 @@ from __future__ import absolute_import
 #
 # Author(s): Sehoon Ha <sehoon.ha@disneyresearch.com>
 # Disney Research Robotics Group
-
-from pydart2.gui import opengl
-assert(opengl)
+import pydart2.gui
+assert(pydart2.gui.opengl)
 
 try:
     from pydart2.gui import glut
